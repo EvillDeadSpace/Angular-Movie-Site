41903909893f174b582db0629235c4f4d8700f5c
Amar Tubic <73004912+EvillDeadSpace@users.noreply.github.com>
2024-12-07 21:51:10 +0100

    Init Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { MovieComponent } from './movie/movie.component';
+import { HeaderComponent } from './header/header.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    AboutComponent,
+    MovieComponent,
+    RouterLink,
+    HeaderComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Projekat_1';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+import { MovieComponent } from './movie/movie.component';
+import { HomeComponent } from './home/home.component';
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'movie', component: MovieComponent },
+];
