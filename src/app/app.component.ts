066f4533788bfb6f91c913f5f6d46b84235180e4
Amar Tubic <73004912+EvillDeadSpace@users.noreply.github.com>
2024-12-11 01:19:46 +0100

    First commit dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { DarkModeService } from './services/dark-mode.service';
+import { Component, HostBinding, inject, Inject, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { MovieComponent } from './movie/movie.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { TrendingComponent } from './trending/trending.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -16,10 +18,18 @@ import { TrendingComponent } from './trending/trending.component';
     HeaderComponent,
     FooterComponent,
     TrendingComponent,
+    CommonModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Projekat_1';
+
+  darkMode = signal<boolean>(false);
+
+  @HostBinding('class.white') get mode() {
+    return this.darkMode();
+  }
+  DarkModeService: DarkModeService = inject(DarkModeService);
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DarkModeService } from './../services/dark-mode.service';
+import { Component, HostBinding, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  darkModeService: DarkModeService = inject(DarkModeService);
+
+  whiteMode = signal<boolean>(false);
+
+  @HostBinding('class.dark') get mode() {
+    return this.whiteMode();
+  }
+
+  toggleDarkMode() {
+    this.darkModeService.updateDarkMode();
+  }
+}
